d85b8a7f0c33967da1477eb568fed4b05dc15bce
Aiden Hong <brain1401@gmail.com>
2023-08-15 02:45:55 +0900

    모바일 반응성 개선

--- src/app/components/ui/LoginButton.tsx
+++ src/app/components/ui/LoginButton.tsx
@@ -4,11 +4,14 @@ type Props = {
   type: "login" | "logout";
 };
 
-const BUTTON_CLASSNAME = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded";
+const BUTTON_CLASSNAME =
+  "bg-blue-500 hover:bg-blue-700 text-white py-[.3rem] px-10 m-1 font-bold px-[0.3rem] md:py-2 md:px-4 rounded";
 
 export default function LoginButton({ type }: Props) {
   return type === "login" ? (
-    <button onClick={() => signIn()} className={BUTTON_CLASSNAME}>로그인</button>
+    <button onClick={() => signIn()} className={BUTTON_CLASSNAME}>
+      로그인
+    </button>
   ) : (
     <button onClick={() => signOut()} className={BUTTON_CLASSNAME}>
       로그아웃
