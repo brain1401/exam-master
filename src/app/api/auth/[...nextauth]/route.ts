225a6c195d88c0adb3a560bb154d1e0386aadcd7
Aiden Hong <brain1401@gmail.com>
2023-08-16 11:13:26 +0900

    전반적인 로직 개선

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import { createIfUserNotExitst } from "@/service/user";
+import { createUserIfNotExists } from "@/service/user";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -11,15 +11,33 @@ const handler = NextAuth({
     }),
   ],
   callbacks: {
-    async signIn({user})
-    // 유저가 로그인을 시도했을 때 실행되는 콜백함수
-    {
-      const result = await createIfUserNotExitst(user.email || "", user.name || "", user.image || "")
-      
-      return result;
-    }
-  }
+    async signIn({ user }) {
+      // 유저가 로그인을 시도했을 때 실행되는 콜백함수
+      try {
+        if (
+          !process.env.NEXTAUTH_SECRET ||
+          !process.env.GOOGLE_CLIENT_ID ||
+          !process.env.GOOGLE_CLIENT_SECRET
+        ) {
+          throw new Error("Environment variables not set");
+        }
 
+        if (!user.email || !user.name) {
+          throw new Error("Invalid user data");
+        }
+
+        const result = await createUserIfNotExists(
+          user.email,
+          user.name,
+          user.image || ""
+        ); // 유저가 없으면 생성하고 있으면 그냥 넘어감
+        return result;
+      } catch (error) {
+        console.error("signIn callback 중 에러:", error);
+        return false; // 로그인 실패
+      }
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
