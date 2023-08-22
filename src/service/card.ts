33d7be306e6aa3231da122e6ef76c26f18899ed0
Aiden Hong <brain1401@gmail.com>
2023-08-23 01:55:19 +0900

    card가 비어있는지 확인하는 함수 추가

---
+++ src/service/card.ts
@@ -0,0 +1,18 @@
+import { Card } from "@/app/types/card";
+
+export const isCardEmpty = (card: Card) => {
+  if (card.question === "") {
+    console.log("question is empty");
+    return true;
+  }
+
+  if (
+    card.candidates === null ||
+    card.candidates.some((candidate) => candidate.text === "")
+  ) {
+    console.log("candidate is empty");
+    return true;
+  }
+
+  return false;
+};
