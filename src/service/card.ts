2c549b185605b136818c407c3b8a9e15c179eace
Aiden Hong <brain1401@gmail.com>
2023-08-24 03:12:29 +0900

    문제가 작성중인지 판단하는 함수 추가

--- src/service/card.ts
+++ src/service/card.ts
@@ -18,5 +18,34 @@ export const isCardEmpty = (card: Card) => {
     console.log("answer is empty");
     return true;
   }
+
+  if(card.type === "sub" && card.subAnswer === "") {
+    console.log("subAnswer is empty");
+    return true;
+  }
+
   return false;
 };
+
+export const isCardOnBeingWrited = (card: Card) => {
+  if (card.question !== "") {
+    console.log("question is not empty");
+    return true;
+  }
+
+  if (
+    card.candidates !== null  &&
+    card.candidates.some((candidate) => candidate.text !== "")
+  ) {
+    console.log("candidate is not empty");
+    return true;
+  }
+
+  if(card.type === "sub" && card.subAnswer !== "") {
+    console.log("subAnswer is not empty");
+    return true;
+  }
+
+  
+  return false;
+}
