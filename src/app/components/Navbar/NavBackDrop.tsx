671d0b24871ac4232fc12e89d87256c945657ff8
Aiden Hong <brain1401@gmail.com>
2023-08-25 03:14:41 +0900

    Navbar에서 분리

---
+++ src/app/components/Navbar/NavBackDrop.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { useAtom } from "jotai";
+import { isNavbarMenuOpenAtom } from "@/app/jotai/store";
+
+export default function NavBackDrop() {
+  const [isMenuOpen, setIsMenuOpen] = useAtom(isNavbarMenuOpenAtom);
+  return (
+    <div // backdrop
+      className={`${
+        isMenuOpen ? "block md:hidden" : "hidden"
+      } fixed top-0 left-0 w-full h-full bg-black opacity-50 z-5 backdrop-blur-md`}
+      onClick={() => setIsMenuOpen(false)}
+    ></div>
+  );
+}
