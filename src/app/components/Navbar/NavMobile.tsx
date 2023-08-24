4cf45aba7d572ff21432fba852ffacc24241d659
Aiden Hong <brain1401@gmail.com>
2023-08-25 03:15:00 +0900

    Navbar에서 분리

---
+++ src/app/components/Navbar/NavMobile.tsx
@@ -0,0 +1,71 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "../../../../public/Exam Master.svg";
+import { useRef, useEffect } from "react";
+import { isNavbarMenuOpenAtom } from "@/app/jotai/store";
+import { useAtom } from "jotai";
+
+const MOBILE_LIST_ITEM = "py-2 w-full border-b border-gray-300 text-center";
+
+type Props = {
+  loginButton: JSX.Element;
+};
+export default function NavMobile({ loginButton }: Props) {
+  const menuRef = useRef<HTMLDivElement>(null);
+  const [isMenuOpen, setIsMenuOpen] = useAtom(isNavbarMenuOpenAtom);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsMenuOpen(false);
+      }
+    }
+
+    if (isMenuOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isMenuOpen]);
+
+  return (
+    <div // mobile menu
+      ref={menuRef}
+      className={`${
+        isMenuOpen
+          ? "opacity-100 pointer-events-auto md:hidden translate-x-0"
+          : "opacity-0 pointer-events-none max-h-0 translate-x-full overflow-hidden"
+      } absolute w-3/4 h-screen bg-neutral-100 top-0 right-0 z-10 transition-all duration-300 ease-in-out`}
+    >
+      <ul className="flex flex-col justify-center items-center w-full">
+        <li className="flex justify-center items-center border-b border-gray-300 w-full py-2 ">
+          <Image src={Logo} alt="logo" className="w-[8rem] h-[2rem]" />
+        </li>
+        <li className={MOBILE_LIST_ITEM}>
+          <Link href="/exam" onClick={() => setIsMenuOpen(false)}>
+            문제 풀기
+          </Link>
+        </li>
+        <li className={MOBILE_LIST_ITEM}>
+          <Link href="/manage" onClick={() => setIsMenuOpen(false)}>
+            문제 관리
+          </Link>
+        </li>
+        <li className={MOBILE_LIST_ITEM}>
+          <Link href="/create" onClick={() => setIsMenuOpen(false)}>
+            문제 생성
+          </Link>
+        </li>
+        <li className="py-2" onClick={() => setIsMenuOpen(false)}>
+          {loginButton}
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/app/components/Navbar/PCNavbar.tsx
@@ -0,0 +1,48 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "../../../../public/Exam Master.svg";
+import { useAtom } from "jotai";
+import { isNavbarMenuOpenAtom } from "@/app/jotai/store";
+import { HiMenu } from "react-icons/hi";
+
+const LIST_ITEM =
+  "hidden md:flex text-md font-bold items-center justtify-center";
+
+type Props = {
+  loginButton: JSX.Element;
+};
+
+export default function PCNavbar({ loginButton }: Props) {
+  const [isMenuOpen, setIsMenuOpen] = useAtom(isNavbarMenuOpenAtom);
+  return (
+    <nav // navbar
+      className="flex border-b border-gray-300"
+    >
+      <ul className="flex justify-center items-cneter md:gap-3">
+        <li className="px-5 py-2 font-bold text-xl md:text-2xl ml-[1rem] md:ml-[3rem]">
+          <Link href="/">
+            <Image src={Logo} alt="logo" className="w-[8rem] h-[2rem]" />
+          </Link>
+        </li>
+        <li className={LIST_ITEM}>
+          <Link href="/exam">문제 풀기</Link>
+        </li>
+        <li className={LIST_ITEM}>
+          <Link href="/manage">문제 관리</Link>
+        </li>
+        <li className={LIST_ITEM}>
+          <Link href="/create">문제 생성</Link>
+        </li>
+        <li className="hidden md:list-item md:absolute md:top-0 md:right-[10vw]">
+          {loginButton}
+        </li>
+        <li className="md:hidden">
+          <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <HiMenu className="text-[1.9rem] absolute top-[0.5rem] right-5" />
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+}
