421e7f28b0aae035f1b042723ccc420faac72510
Aiden Hong <brain1401@gmail.com>
2023-08-25 03:16:24 +0900

    isCardEmpty로직 개선

--- src/service/card.ts
+++ src/service/card.ts
@@ -7,14 +7,13 @@ export const isCardEmpty = (card: Card) => {
   }
 
   if (
-    card.candidates === null ||
-    card.candidates.some((candidate) => candidate.text === "")
+    card.candidates && card.candidates.some((candidate) => candidate.text === "")
   ) {
     console.log("candidate is empty");
     return true;
   }
 
-  if (!card.candidates?.some((candidate) => candidate.isAnswer === true)) {
+  if (card.candidates && !card.candidates?.some((candidate) => candidate.isAnswer === true)) {
     console.log("answer is empty");
     return true;
   }
