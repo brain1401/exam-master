212968d01a0749e4cddf403609026beb74c52711
Aiden Hong <brain1401@gmail.com>
2023-08-25 03:14:23 +0900

    CreateProblems에서 분리

---
+++ src/app/components/CreateProblems/CurrentCardIndicator.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { currentCardIndexAtom, cardsLengthAtom } from "@/app/jotai/store";
+import {useAtomValue} from "jotai";
+
+
+export default function CurrentCardIndicator() {
+
+  const currentIndex = useAtomValue(currentCardIndexAtom);
+  const cardLength = useAtomValue(cardsLengthAtom);
+
+  return (
+    <p className="text-center mb-5 text-3xl">
+      {currentIndex + 1}번째 문제 / 총 {cardLength}개
+    </p>
+  );
+}
