a7bfc9499d6059779108f06a67aba12cb58f6038
Aiden Hong <brain1401@gmail.com>
2023-08-25 03:14:27 +0900

    CreateProblems에서 분리

---
+++ src/app/components/CreateProblems/NextOrPrevButtons.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { useAtomValue, useSetAtom } from "jotai";
+import { currentCardIndexAtom, cardsLengthAtom } from "@/app/jotai/store";
+
+export default function NextOrPrevButtons() {
+  const setCurrentIndex = useSetAtom(currentCardIndexAtom);
+  const maxIndex = useAtomValue(cardsLengthAtom);
+
+  const showNextCard = () => {
+    setCurrentIndex((prevIndex) => {
+      if (prevIndex < parseInt(maxIndex) - 1) {
+        window.scrollTo(0, 0);
+        return prevIndex + 1;
+      } else {
+        return prevIndex;
+      }
+    });
+  };
+
+  const showPreviousCard = () => {
+    setCurrentIndex((prevIndex) => {
+      if (prevIndex !== 0) {
+        window.scrollTo(0, 0);
+        return prevIndex - 1;
+      } else {
+        return prevIndex;
+      }
+    });
+  };
+
+  return (
+    <div className="flex justify-center mt-8 gap-5">
+      <button
+        onClick={showPreviousCard}
+        className="px-5 py-2 border border-black rounded-md hover:bg-slate-300 hover:border-slate-300"
+      >
+        이전
+      </button>
+      <button
+        onClick={showNextCard}
+        className="px-5 py-2 border border-black rounded-md hover:bg-slate-300 hover:border-slate-300"
+      >
+        다음
+      </button>
+    </div>
+  );
+}
