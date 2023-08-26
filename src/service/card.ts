082a894c073403e4f26d76c614c70d6f30f0b7ec
Aiden Hong <brain1401@gmail.com>
2023-08-27 01:22:37 +0900

    함수에 단일 책임 원칙 적용

--- src/service/card.ts
+++ src/service/card.ts
@@ -70,82 +70,19 @@ export const isCardOnBeingWrited = (card: Card) => {
   return false;
 };
 
-export async function postProblems(
-  setName: string,
-  userEmail: string,
-  cards: Card[]
-) {
-  checkEnvVariables();
-  const postedId: string[] = [];
-
-  for (const card of cards) {
-    if (isCardEmpty(card)) {
-      throw new Error("문제가 비어있습니다.");
-    }
-
-    let postId: string = "";
-
-    try {
-      // 문제 생성
-      const response = await axios.post(
-        `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/exam-problems`,
-        {
-          questionType: card.type,
-          question: card.question,
-          additionalView: card.additionalView,
-          candidates: card.candidates,
-          subjectiveAnswer: card.subAnswer,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
-          },
-        }
-      );
-
-      postId = response.data.data.id;
-      postedId.push(postId);
-      
-    } catch (err) {
-      console.log(err);
-      throw new Error("문제를 업로드하는 중 오류가 발생했습니다.");
-    }
-
-    try {
-      // 위에서 생성한 문제에 사진 넣기
-      const newFormData = new FormData();
-      newFormData.append("files", card.image as Blob);
-      newFormData.append("ref", "api::exam-problem.exam-problem");
-      newFormData.append("refId", postId);
-      newFormData.append("field", "image");
-      await axios.post(
-        `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/upload`,
-        newFormData,
-        {
-          headers: {
-            Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
-    } catch (err) {
-      console.log(err);
-      throw new Error("문제를 업로드하는 중 오류가 발생했습니다.");
-    }
-  }
-  console.log("postedId :",postedId);
+export async function createProblem(card: Card, postedId: string[]):Promise<string> {
   try {
-    const user = await getUser(userEmail);
+    // 문제 생성
+    let postId = "";
+
     const response = await axios.post(
-      `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/exam-problem-sets`,
+      `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/exam-problems`,
       {
-        name: setName,
-        exam_problems: {
-          connect: [...postedId],
-        },
-        exam_users: {
-          connect: [user.id],
-        },
+        questionType: card.type,
+        question: card.question,
+        additionalView: card.additionalView,
+        candidates: card.candidates,
+        subjectiveAnswer: card.subAnswer,
       },
       {
         headers: {
@@ -153,10 +90,88 @@ export async function postProblems(
         },
       }
     );
-    console.log(response.data);
-    return response.data.data;
+
+    postId = response.data.data.id;
+    postedId.push(postId);
+    return postId;
   } catch (err) {
     console.log(err);
-    throw new Error("문제를 업로드하는 중 오류가 발생했습니다.");
+    throw new Error("문제를 생성하는 중 오류가 발생했습니다.");
   }
+}
+
+export async function createImage(card: Card, postId: string){
+   try {
+     // 위에서 생성한 문제에 사진 넣기
+     const newFormData = new FormData();
+     newFormData.append("files", card.image as Blob);
+     newFormData.append("ref", "api::exam-problem.exam-problem");
+     newFormData.append("refId", postId);
+     newFormData.append("field", "image");
+     await axios.post(
+       `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/upload`,
+       newFormData,
+       {
+         headers: {
+           Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
+           "Content-Type": "multipart/form-data",
+         },
+       }
+     );
+   } catch (err) {
+     console.log(err);
+     throw new Error("이미지를 생성하는 중 오류가 발생했습니다.");
+   }
+}
+
+export async function createProblemSets(userEmail: string, setName: string, postIdArray: string[]) {
+    try {
+      const user = await getUser(userEmail);
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/exam-problem-sets`,
+        {
+          name: setName,
+          exam_problems: {
+            connect: [...postIdArray],
+          },
+          exam_users: {
+            connect: [user.id],
+          },
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
+          },
+        }
+      );
+
+      return response.data.data;
+    } catch (err) {
+      console.log(err);
+      throw new Error("문제를 업로드하는 중 오류가 발생했습니다.");
+    }
+}
+
+export async function postProblems(
+  setName: string,
+  userEmail: string,
+  cards: Card[]
+) {
+  checkEnvVariables();
+  const postIdArray: string[] = [];
+
+  for (const card of cards) {
+    if (isCardEmpty(card)) {
+      throw new Error("문제가 비어있습니다.");
+    }
+
+    let postId = await createProblem(card, postIdArray);
+    card.image && await createImage(card, postId);
+
+  }
+  console.log("postedId :",postIdArray);
+
+  const response = await createProblemSets(userEmail, setName, postIdArray);
+  return response;
+
 }
