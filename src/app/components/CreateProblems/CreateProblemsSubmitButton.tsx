afb8b51dfe14181de72e9060916f786c7f3fcea5
Aiden Hong <brain1401@gmail.com>
2023-08-29 08:34:36 +0900

    문제집 제출버튼 눌렀을 때 제출 로딩 로직 추가

--- src/app/components/CreateProblems/CreateProblemsSubmitButton.tsx
+++ src/app/components/CreateProblems/CreateProblemsSubmitButton.tsx
@@ -1,10 +1,16 @@
 "use client";
+import { useState } from "react";
 import { useAtomValue, useAtom, useSetAtom } from "jotai";
-import { problemsSetsNameAtom, cardsAtom, resetCardsAtom } from "@/app/jotai/store";
+import {
+  problemsSetsNameAtom,
+  cardsAtom,
+  resetCardsAtom,
+} from "@/app/jotai/store";
 import { isCardEmpty } from "@/service/card";
 import { useRouter } from "next/navigation";
 
 export default function CreateProblemsSubmitButton() {
+  const [isLoading, setIsLoading] = useState(false);
   const cards = useAtomValue(cardsAtom);
   const resetCards = useSetAtom(resetCardsAtom);
   const [problemSetName, setProblemSetName] = useAtom(problemsSetsNameAtom);
@@ -20,37 +26,49 @@ export default function CreateProblemsSubmitButton() {
       return;
     }
 
+    if (!confirm("문제집을 제출하시겠습니까?")) return;
+
+    setIsLoading(true); // 로딩 시작
+
     const formData = new FormData();
     cards.forEach((card, index) => {
       formData.append(`image[${index}]`, card.image as Blob);
       formData.append(`data[${index}]`, JSON.stringify(card));
     });
     formData.append("problemSetName", problemSetName);
-    const response = await fetch("/api/postProblems", {
-      method: "POST",
-      body: formData,
-    });
 
-    const data = await response.json();
-    if (data === "OK") {
-      alert("문제집이 성공적으로 등록되었습니다.");
-      resetCards();
-      setProblemSetName("");
+    try {
+      const response = await fetch("/api/postProblems", {
+        method: "POST",
+        body: formData,
+      });
 
-      router.refresh();
+      const data = await response.json();
+      if (data === "OK") {
+        alert("문제집이 성공적으로 등록되었습니다.");
+        resetCards();
+        setProblemSetName("");
+        router.refresh();
+      } else {
+        alert("문제집 등록에 실패했습니다.");
+      }
     }
-    else{
+    catch(err) {
       alert("문제집 등록에 실패했습니다.");
+    }
+     finally {
+      setIsLoading(false); // 로딩 완료
     }
   };
 
   return (
     <div className="flex justify-center mt-3">
       <button
-        className="px-2 py-2 md:px-5 md:py-2 bg-blue-500 rounded-md text-white "
+        className="px-2 py-2 md:px-5 md:py-2 bg-blue-500 rounded-md text-white"
         onClick={handleSubmit}
+        disabled={isLoading}
       >
-        최종제출
+        {isLoading ? "제출 중..." : "최종제출"}
       </button>
     </div>
   );
