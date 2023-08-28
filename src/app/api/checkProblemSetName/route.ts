869e687c2b33be6540745d879185224b1d9f1fb8
Aiden Hong <brain1401@gmail.com>
2023-08-29 08:32:26 +0900

    유저가 이미 문제집을 만들었는지 검사하는 api 생성

---
+++ src/app/api/checkProblemSetName/route.ts
@@ -0,0 +1,21 @@
+import { checkProblemSetName } from "@/service/card";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const session = await getServerSession();
+  if (!session) {
+    return NextResponse.json(
+      { error: "로그인이 필요합니다." },
+      { status: 401 }
+    );
+  }
+
+  const params = req.nextUrl.searchParams;
+  const name = params.get("name");
+  const userEmail = session?.user?.email ?? "";
+
+  const result = await checkProblemSetName(name ?? "", userEmail);
+
+  return NextResponse.json(result);
+}
