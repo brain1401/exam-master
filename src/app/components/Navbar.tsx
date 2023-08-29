5d818c7f2167cabc74e4be5882f1296bf55db36d
Aiden Hong <brain1401@gmail.com>
2023-08-29 23:16:09 +0900

    header 시멘틱 태그 추가

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,8 +5,7 @@ import NavBackDrop from "./Navbar/NavBackDrop";
 import PCNavbar from "./Navbar/PCNavbar";
 
 export default async function Navbar() {
-  const session  = await getServerSession();
- 
+  const session = await getServerSession();
 
   const loginButton = session ? (
     <LoginButton type="logout" />
@@ -14,14 +13,11 @@ export default async function Navbar() {
     <LoginButton type="login" />
   );
 
-
   return (
-    <>
-    <NavMobile loginButton={loginButton} />
-    <NavBackDrop/>
-    <PCNavbar loginButton={loginButton} />
-
-      
-    </>
+    <header>
+      <NavMobile loginButton={loginButton} />
+      <NavBackDrop />
+      <PCNavbar loginButton={loginButton} />
+    </header>
   );
 }
