5a10ec344344d2fa3bfc9015be1656702b54bf4f
Aiden Hong <brain1401@gmail.com>
2023-08-30 00:18:01 +0900

    클라이언트 컴포넌트로 바꾸고 언마운트시 카드 전역 상태값 초기화

--- src/app/components/CreateProblems.tsx
+++ src/app/components/CreateProblems.tsx
@@ -1,10 +1,22 @@
+"use client";
 import CreateProblemsCard from "./CreateProblemsCard";
 import CreateProblemsOption from "./CreateProblems/CreateProblemsOption";
 import NextOrPrevButtons from "./CreateProblems/NextOrPrevButtons";
 import CurrentCardIndicator from "./CreateProblems/CurrentCardIndicator";
 import CreateProblemsSubmitButton from "./CreateProblems/CreateProblemsSubmitButton";
+import { useEffect } from "react";
+import { resetCardsAtom } from "../jotai/store";
+import { useSetAtom } from "jotai";
 
 export default function CreateProblems() {
+  const resetCard = useSetAtom(resetCardsAtom);
+
+  useEffect(() => {
+    return () => {
+      resetCard();
+    };
+  }, [resetCard]);
+
   return (
     <section className="p-3 mt-10 max-w-[70rem] mx-auto">
       <CreateProblemsOption />
