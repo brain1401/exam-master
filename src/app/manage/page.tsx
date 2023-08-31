03e1effaec42ef1be93ebab7acf29b0c561c7b3c
Aiden Hong <brain1401@gmail.com>
2023-09-01 03:39:23 +0900

    ProblemSetGrid 컴포넌트 사용

--- src/app/manage/page.tsx
+++ src/app/manage/page.tsx
@@ -1,14 +1,10 @@
 import { getServerSession } from "next-auth";
+import ProblemSetGrid from "../components/ProblemSetGrid";
 
 export default async function ManagePage() {
   const session = await getServerSession();
-  return (
-    <>
-      {session ? (
-        <h1>환영합니다. {session.user?.name}님!</h1>
-      ) : (
-        <h1>로그인이 필요합니다.</h1>
-      )}
-    </>
-  );
+
+  return (<>{
+    session ? <ProblemSetGrid/> : <h1>로그인이 필요합니다.</h1>
+    }</>);
 }
