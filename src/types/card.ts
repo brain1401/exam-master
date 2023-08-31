84985b8438ccf93ff203ade1c6d50451e9b9b72e
Aiden Hong <brain1401@gmail.com>
2023-09-01 03:35:37 +0900

    ProblemSet 타입 추가

--- src/types/card.ts
+++ src/types/card.ts
@@ -13,3 +13,11 @@ export type Card = {
   candidates: candidate[] | null;
   subAnswer: string | null;
 } | null;
+
+export type ProblemSet = {
+  id: string;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+  UUID: string;
+}
