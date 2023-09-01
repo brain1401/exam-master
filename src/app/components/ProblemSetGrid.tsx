8f955d1d2881e9315411359cad449f8aabbd640d
Aiden Hong <brain1401@gmail.com>
2023-09-01 18:26:36 +0900

    다양한 input 추가

--- src/app/components/ProblemSetGrid.tsx
+++ src/app/components/ProblemSetGrid.tsx
@@ -6,44 +6,54 @@ import {
 } from "../jotai/manageProblems";
 import ProblemSetCard from "./ProblemSetCard";
 import { ProblemSet } from "@/types/card";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import Button from "./ui/Button";
+import { BsSearch } from "react-icons/bs";
 
 export default function ProblemSetGrid() {
   const problemSets = useAtomValue(problemSetAtomsWithQuery);
   const setCurrentPage = useSetAtom(problemSetCurrentPageAtom);
   const maxPage = useRef(problemSets?.meta.pagination.pageCount ?? 0);
 
+  const [searchString, setSearchString] = useState("");
+
   useEffect(() => {
-    
     return () => {
       setCurrentPage(1);
-    }
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[])
+  }, []);
 
   return (
     <section>
-      <ul className="flex flex-wrap">
+      <div className="flex justify-end items-center h-10">
+        <div className="flex justify-center items-center">
+          <input
+            type="text"
+            className="border border-black rounded-md py-1 mx-auto"
+            name="search"
+            id="search"
+            placeholder="검색어를 입력하세요."
+            value={searchString}
+            onChange={(e) => setSearchString(e.target.value)}
+          />
+          <button className="ml-1 p-2 justify-center items-center text-center">
+            <BsSearch />
+          </button>
+        </div>
+      </div>
+
+      <ul className="grid grid-cols-5 w-[55rem] mx-auto">
         {problemSets?.data.map((problemSet: ProblemSet) => (
-          <li key={problemSet.UUID}>
+          <li key={problemSet.UUID} className="flex justify-center">
             <ProblemSetCard problemSet={problemSet} />
           </li>
         ))}
       </ul>
 
-      <div>
-        <button
-          onClick={async () => {
-            setCurrentPage((prev) => {
-              if (prev === maxPage.current ) return prev;
-              return prev + 1;
-            });
-          }}
-        >
-          다음
-        </button>
-        <button
-          onClick={async () => {
+      <div className="flex gap-3 justify-center mt-5">
+        <Button
+          onClick={() => {
             setCurrentPage((prev) => {
               if (prev === 1) return prev;
               return prev - 1;
@@ -51,7 +61,18 @@ export default function ProblemSetGrid() {
           }}
         >
           이전
-        </button>
+        </Button>
+
+        <Button
+          onClick={() => {
+            setCurrentPage((prev) => {
+              if (prev === maxPage.current) return prev;
+              return prev + 1;
+            });
+          }}
+        >
+          다음
+        </Button>
       </div>
     </section>
   );
