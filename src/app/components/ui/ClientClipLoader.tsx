e0b643bb889e6f3af77610c80a00edafdd9fd911
Aiden Hong <brain1401@gmail.com>
2023-09-01 16:57:26 +0900

    ClipLoader use client로 감싼 컴포넌트 생성

---
+++ src/app/components/ui/ClientClipLoader.tsx
@@ -0,0 +1,13 @@
+"use client";
+import {ClipLoader} from "react-spinners";
+
+type Props = {
+  size?: number;
+  color?: string;
+  className?: string;
+}
+export default function ClientClipLoader({size, color, className}: Props) {
+  return (
+    <ClipLoader size={size} color={color} className={className}/>
+  )
+}
