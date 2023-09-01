e6312e35dd705007e0e6cd8b12c78831bf1b7896
Aiden Hong <brain1401@gmail.com>
2023-09-01 18:26:04 +0900

    카드 스타일링

--- src/app/components/ProblemSetCard.tsx
+++ src/app/components/ProblemSetCard.tsx
@@ -4,12 +4,13 @@ type Props = {
   problemSet: ProblemSet;
 };
 
-export default function ProblemSetCard({problemSet} : Props) {
+export default function ProblemSetCard({ problemSet }: Props) {
   return (
-    <div className="my-2">
-      <h1>{problemSet.name}</h1>
-      <p>{problemSet.createdAt}</p>
+    <div className="border border-black flex flex-col items-center w-[10rem] my-2 rounded-lg p-5">
+      <h1 className="font-bold text-ellipsis overflow-hidden whitespace-nowrap w-full text-center">
+        {problemSet.name}
+      </h1>
+      <p>{problemSet.updatedAt}</p>
     </div>
-
-  )
+  );
 }
