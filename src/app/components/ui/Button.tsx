f2ae43f413ae392706f33da1d959aefa0faf7648
Aiden Hong <brain1401@gmail.com>
2023-09-01 18:25:39 +0900

    Button 컴포넌트 생성

---
+++ src/app/components/ui/Button.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { twMerge } from "tailwind-merge";
+type Props = {
+  children: string;
+  className?: string;
+  onClick?: () => void;
+  disabled?: boolean;
+};
+export default function Button({
+  children,
+  className,
+  onClick,
+  disabled = false,
+}: Props) {
+  return (
+    <button
+      className={twMerge(
+        `border border-black px-5 py-2 rounded-md hover:bg-neutral-500 hover:text-white hover:border-transparent transition-colors duration-300 hover:border-neutral-500`,
+        className
+      )}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
