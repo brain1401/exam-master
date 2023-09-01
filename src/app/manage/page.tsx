b16406bf5f9c5d6367803b4fe889250a1b45579b
Aiden Hong <brain1401@gmail.com>
2023-09-01 16:56:45 +0900

    그리드 추가

--- src/app/manage/page.tsx
+++ src/app/manage/page.tsx
@@ -1,10 +1,30 @@
 import { getServerSession } from "next-auth";
 import ProblemSetGrid from "../components/ProblemSetGrid";
+import { Suspense } from "react";
+import ClientClipLoader from "../components/ui/ClientClipLoader";
 
 export default async function ManagePage() {
   const session = await getServerSession();
 
-  return (<>{
-    session ? <ProblemSetGrid/> : <h1>로그인이 필요합니다.</h1>
-    }</>);
+  return (
+    <>
+      {session ? (
+        <section className="p-3">
+          <h1>문제집 관리</h1>
+
+          <Suspense
+            fallback={
+              <div className="w-full flex justify-center items-center">
+                <ClientClipLoader size={50}/>
+              </div>
+            }
+          >
+            <ProblemSetGrid />
+          </Suspense>
+        </section>
+      ) : (
+        <h1>로그인이 필요합니다.</h1>
+      )}
+    </>
+  );
 }
