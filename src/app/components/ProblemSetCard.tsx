551d8b32aae400b3d55a9022b0c55332858b9687
Aiden Hong <brain1401@gmail.com>
2023-09-03 09:47:55 +0900

    Link 태그 추가

--- src/app/components/ProblemSetCard.tsx
+++ src/app/components/ProblemSetCard.tsx
@@ -1,4 +1,5 @@
 import { ProblemSet } from "@/types/card";
+import Link from "next/link";
 
 type Props = {
   problemSet: ProblemSet;
@@ -8,11 +9,13 @@ export default function ProblemSetCard({ problemSet }: Props) {
   const formattedDate = new Date(problemSet.updatedAt).toLocaleDateString();
 
   return (
-    <div className="border border-gray-300 flex flex-col items-center w-full md:w-[10rem] my-2 rounded-lg p-5 hover:shadow-lg transition-shadow duration-200 ease-in cursor-pointer">
-      <h1 className="font-bold text-lg text-gray-700 truncate w-full text-center mb-2">
-        {problemSet.name}
-      </h1>
-      <p className="text-sm text-gray-500">{formattedDate}</p>
-    </div>
+    <Link href={`/manage/${problemSet.UUID}`}>
+      <div className="border border-gray-300 flex flex-col items-center w-full md:w-[10rem] my-2 rounded-lg p-5 hover:shadow-lg transition-shadow duration-200 ease-in cursor-pointer">
+        <h1 className="font-bold text-lg text-gray-700 truncate w-full text-center mb-2">
+          {problemSet.name}
+        </h1>
+        <p className="text-sm text-gray-500">{formattedDate}</p>
+      </div>
+    </Link>
   );
 }
