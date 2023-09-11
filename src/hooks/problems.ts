a84643386a3465befc41aa207f46476629c3a892
Aiden Hong <brain1401@gmail.com>
2023-09-11 20:56:14 +0900

    problems 커스텀 훅 생성

--- src/hooks/problems.ts
+++ src/hooks/problems.ts
@@ -1,4 +1,4 @@
-import { Problem } from "@/types/problems";
+import { Problem, candidate } from "@/types/problems";
 import { useState } from "react";
 
 export default function useProblems() {
@@ -9,7 +9,24 @@ export default function useProblems() {
   const [problemSetsName, setProblemSetsName] = useState<string>("");
 
   const resetProblems = () => {
-    setProblems(Array<Problem>(10).fill(null));
+    setProblems(
+      Array<Problem>(10)
+        .fill(null)
+        .fill(
+          {
+            type: "obj",
+            question: "",
+            additionalView: "",
+            isAdditiondalViewButtonClicked: false,
+            isImageButtonClicked: false,
+            image: null,
+            candidates: Array<candidate>(4).fill({ text: "", isAnswer: false }),
+            subAnswer: null,
+          },
+          0,
+          1
+        )
+    );
     setProblemCurrentIndex(0);
   };
 
