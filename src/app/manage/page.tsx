1725f89c3cca3e3510ab7f91cf7d2a6cf6a78540
Aiden Hong <brain1401@gmail.com>
2023-09-14 13:37:59 +0900

    suspense 제거

--- src/app/manage/page.tsx
+++ src/app/manage/page.tsx
@@ -1,7 +1,5 @@
 import { getServerSession } from "next-auth";
 import ProblemSetGrid from "../components/ProblemSetGrid";
-import { Suspense } from "react";
-import ClientClipLoader from "../components/ui/ClientClipLoader";
 
 export default async function ManagePage() {
   const session = await getServerSession();
@@ -11,16 +9,7 @@ export default async function ManagePage() {
       {session ? (
         <section className="p-3 max-w-[80rem] mx-auto">
           <h1>문제집 관리</h1>
-
-          <Suspense
-            fallback={
-              <div className="mt-[8rem] w-full text-center">
-                <ClientClipLoader size={50}/>
-              </div>
-            }
-          >
             <ProblemSetGrid />
-          </Suspense>
         </section>
       ) : (
         <h1>로그인이 필요합니다.</h1>
