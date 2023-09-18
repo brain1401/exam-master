a723b8fe83bb467da5b0c6d921d53bca9da2718a
Aiden Hong <brain1401@gmail.com>
2023-09-18 15:11:57 +0900

    Create -> manage로 코드 및 컴포넌트 변경

--- src/app/manage/[UUID]/page.tsx
+++ src/app/manage/[UUID]/page.tsx
@@ -2,12 +2,12 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import ProblemsEditor from "@/app/components/ProblemsEditor";
-import NextOrPrevButtons from "@/app/components/CreateProblems/NextOrPrevButtons";
-import CreateProblemsSubmitButton from "@/app/components/CreateProblems/CreateProblemsSubmitButton";
+import NextOrPrevButtons from "@/app/components/CreateProblems/NextOrPrevButtons";;
 import CurrentCardIndicator from "@/app/components/CreateProblems/CurrentCardIndicator";
 import { ClipLoader } from "react-spinners";
 import useProblems from "@/hooks/problems";
 import EditProblemsOption from "@/app/components/CreateProblems/EditProblemsOption";
+import ManageProblemSubmitButton from "@/app/components/ManageProblemSubmitButton";
 type Props = {
   params: {
     UUID: string;
@@ -17,6 +17,7 @@ type Props = {
 export default function EditProblemsByUUID({ params }: Props) {
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(true);
+
   const {
     problemCurrentIndex,
     problemSetsName,
@@ -66,7 +67,7 @@ export default function EditProblemsByUUID({ params }: Props) {
   }
 
   return (
-    <section className="mt-10">
+    <section className="mt-10 p-3 max-w-[80rem] mx-auto">
       <EditProblemsOption
         problems={problems}
         setProblems={setProblems}
@@ -87,11 +88,12 @@ export default function EditProblemsByUUID({ params }: Props) {
         problemLength={problems.length}
         setCurrentProblemIndex={setProblemCurrentIndex}
       />
-      <CreateProblemsSubmitButton
-        problemSetName={problemSetsName}
+      <ManageProblemSubmitButton
         problems={problems}
+        problemSetName={problemSetsName}
         setProblemSetsName={setProblemSetsName}
         resetProblems={resetProblems}
+        UUID={params.UUID}
       />
     </section>
   );
