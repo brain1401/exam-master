ef7d14d3cfdd33e89dc4e3bc29a578c0fe85d0aa
Aiden Hong <brain1401@gmail.com>
2023-09-19 11:08:09 +0900

    api route 생성

---
+++ src/app/api/updateProblems/route.ts
@@ -0,0 +1,62 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { Problem } from "@/types/problems";
+import { postProblems, updateProblems } from "@/service/problems";
+
+export async function POST(req: NextRequest) {
+  const session = await getServerSession();
+
+  if (!session) {
+    return NextResponse.json(
+      { error: "로그인이 필요합니다." },
+      { status: 401 }
+    );
+  }
+
+  const formData = await req.formData();
+  const entries = Array.from(formData.entries());
+
+  const intermediateResults: NonNullable<Problem>[] = [];
+  let problemSetName: string | undefined;
+
+  for (const [name, value] of entries) {
+    if (name === "problemSetName") {
+      problemSetName = value as string;
+      continue;
+    }
+
+    const match = name.match(/(data|image)\[(\d+)\]/);
+    if (match) {
+      const [, prefix, indexStr] = match;
+      const index = parseInt(indexStr);
+
+      if (!intermediateResults[index]) {
+        intermediateResults[index] = {} as NonNullable<Problem>;
+      }
+
+      if (prefix === "data") {
+        const cardData = JSON.parse(value as string);
+        intermediateResults[index] = {
+          ...intermediateResults[index],
+          ...cardData,
+        };
+      } else if (prefix === "image" && value !== "null") {
+        intermediateResults[index].image = value as File;
+      }
+    }
+  }
+
+  if (!problemSetName || !intermediateResults ) {
+    return NextResponse.json(
+      { error: "문제를 생성하는 중 오류가 발생했습니다." },
+      { status: 500 }
+    );
+  }
+  
+  const result = await updateProblems(
+    problemSetName,
+    intermediateResults,
+  );
+
+  return NextResponse.json(result ? "OK" : "NO", { status: 200 });
+}
