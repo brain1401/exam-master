2842a092d55249cf38486fd43d6f2555c3c84803
Aiden Hong <brain1401@gmail.com>
2023-11-07 00:44:36 +0900

    선택지 5번째 이상 등장 시 동그라미 숫자 표시 안되던 문제 수정

--- src/utils/candidateNumber.ts
+++ src/utils/candidateNumber.ts
@@ -14,6 +14,12 @@ export default function candidateNumber(number:number) {
     case 4:
       result = "④";
       break;
+    case 5:
+      result = "⑤";
+      break;
+    case 6:
+      result = "⑥";
+      break;
   }
 
   return result;
