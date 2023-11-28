2a0b219f4f99a4f472b2a7ad2193dea40b8b5a7e
Aiden Hong <brain1401@gmail.com>
2023-11-29 01:31:32 +0900

    indicator 개선

--- src/app/components/result/CurrentProblemIndicator.tsx
+++ src/app/components/result/CurrentProblemIndicator.tsx
@@ -1,10 +1,10 @@
 import useExamProblemResults from "@/hooks/useExamProblemResults";
 
 export default function CurrentProblemIndicator() {
-  const { examProblemResultsIndex, examProblemResults } = useExamProblemResults();
+  const { examProblemResultsIndex, examProblemResults: {length} } = useExamProblemResults();
   return (
-    <h3 className="text-xl font-semibold">
-      {examProblemResultsIndex + 1} / {examProblemResults.length}
+    <h3 className="text-xl font-semibold mb-3">
+      {`${examProblemResultsIndex + 1}번 문제 / 총${length}문제`}
     </h3>
   )
 }
