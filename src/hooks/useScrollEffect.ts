79d2a7ded2e2ec87ffb3c1b06d778c8add89c159
Aiden Hong <brain1401@gmail.com>
2023-11-30 23:03:14 +0900

    혹시모르니 -1000 -> -10000으로 변경

--- src/hooks/useScrollEffect.ts
+++ src/hooks/useScrollEffect.ts
@@ -1,7 +1,7 @@
 import { useLayoutEffect } from "react";
 export default function useScrollEffect(dependencies: any[]) {
   useLayoutEffect(() => {
-    window.scrollTo({ top: -1000, left: 0 });
+    window.scrollTo({ top: -10000, left: 0 });
   }, [dependencies]);
 
   return null;
