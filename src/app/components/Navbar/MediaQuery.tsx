74565a8cf29d32a339cee1d5d9fa2f96044a649d
Aiden Hong <brain1401@gmail.com>
2023-11-30 13:14:52 +0900

    로그인 버튼 안뜨던 현상 수정

---
+++ src/app/components/Navbar/MediaQuery.tsx
@@ -0,0 +1,6 @@
+"use client";
+import useMediaQuery from "@/hooks/useMediaQuery";
+export default function MediaQuery() {
+  useMediaQuery();
+  return <></>;
+}
--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,14 +1,12 @@
-"use client";
 import LoginButton from "../ui/LoginButton";
-import { useSession } from "next-auth/react";
 import NavMobile from "./NavMobile";
 import NavBackDrop from "./NavBackDrop";
 import PCNavbar from "./PCNavbar";
-import useCustomMediaQuery from "@/hooks/useCustomMediaQuery";
-import { useEffect, useState } from "react";
-import { useMediaQuery } from "react-responsive";
-export default function Navbar() {
-  const session = useSession();
+import { getServerSession } from "next-auth";
+import MediaQuery from "./MediaQuery";
+
+export default async function Navbar() {
+  const session = await getServerSession();
 
   const loginButton = session ? (
     <LoginButton type="logout" />
@@ -16,42 +14,11 @@ export default function Navbar() {
     <LoginButton type="login" />
   );
 
-  const { setMediaQuery } = useCustomMediaQuery();
-
-  // 각 화면 크기 범위에 맞는 min-width와 max-width 설정
-  const xxs = useMediaQuery({
-    query: "(max-width: 367px)",
-  });
-  const xs = useMediaQuery({
-    query: "(min-width: 368px) and (max-width: 639px)",
-  });
-  const sm = useMediaQuery({
-    query: "(min-width: 640px) and (max-width: 668px)",
-  });
-  const md = useMediaQuery({
-    query: "(min-width: 669px) and (max-width: 1023px)",
-  });
-  const lg = useMediaQuery({
-    query: "(min-width: 1024px) and (max-width: 1279px)",
-  });
-  const xl = useMediaQuery({ query: "(min-width: 1280px)" }); // xl 이상의 경우는 max-width가 필요 없음
-
-
-  useEffect(() => {
-    setMediaQuery({
-      isXxs: xxs,
-      isXs: xs,
-      isSm: sm,
-      isMd: md,
-      isLg: lg,
-      isXl: xl,
-    });
-  }, [xxs, xs, sm, md, lg, xl, setMediaQuery]);
-
   return (
     <header>
       <NavMobile loginButton={loginButton} />
       <NavBackDrop />
+      <MediaQuery />
       <PCNavbar loginButton={loginButton} />
     </header>
   );
