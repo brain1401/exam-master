53e8327e317351b32d46a61da1831607dd6ba173
Aiden Hong <brain1401@gmail.com>
2023-11-30 22:28:05 +0900

    useLayoutEffect -> useEffect로 변경

--- src/hooks/useScrollEffect.ts
+++ src/hooks/useScrollEffect.ts
@@ -1,12 +1,7 @@
-import { useLayoutEffect } from "react";
+import { useEffect } from "react";
 export default function useScrollEffect(dependencies: any[]) {
-  useLayoutEffect(() => {
-    const root = document.querySelector("#root");
-    if (root) {
-      window.scrollTo({ top: root.scrollTop })
-    }
-    console.log(root);
-    console.log("scroll to top");
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0 });
   }, [dependencies]);
 
   return null;
