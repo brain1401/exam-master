ff54d24a6e90b115928b7b2a3f6e41372bc3284f
Aiden Hong <brain1401@gmail.com>
2023-12-03 17:13:41 +0900

    vercel 배포 오류 해결

---
+++ src/app/components/results/ResultsGrid.tsx
@@ -0,0 +1,22 @@
+import { ExamResultsWithCount } from "@/types/problems";
+import ResultsCard from "./ResultsCard";
+
+type Props = {
+  results: ExamResultsWithCount[] | undefined;
+};
+
+export default function ResultsGrid({ results }: Props) {
+  return (
+    <>
+      {results && (
+        <ul className="mx-auto mt-10 grid w-full grid-cols-1 gap-x-2 gap-y-5 xs:grid-cols-2 sm:grid-cols-2 sm:p-0 min-[669px]:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+          {results.map((result) => (
+            <li key={result.uuid} className="">
+              <ResultsCard result={result} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
