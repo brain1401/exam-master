cc21ec3d09aae1154f7ceb256aed2a25e6d15f00
Aiden Hong <brain1401@gmail.com>
2023-12-06 17:15:03 +0900

    examProblemResult도 Redux로 교체

--- src/hooks/useExamProblemResults.ts
+++ src/hooks/useExamProblemResults.ts
@@ -1,12 +1,48 @@
-import { useAtom, useSetAtom} from "jotai"
-import * as atoms from "@/jotai/examProblemResult"
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import {
+  reset,
+  selectCurrentExamProblemResult,
+  selectExamProblemResults,
+  selectExamProblemResultsIndex,
+  setCurrentProblemResultAction,
+  setExamProblemResultsAction,
+  setExamProblemResultsIndexAction,
+} from "@/slices/examProblemResults";
+import { ExamProblemResult } from "@/types/problems";
+import { useCallback } from "react";
 
 export default function useExamProblemResults() {
-  const [examProblemResults, setExamProblemResults] = useAtom(atoms.examProblemResultsAtom)
-  const [examProblemResultsIndex, setExamProblemResultsIndex] = useAtom(atoms.examProblemResultsIndexAtom)
-  const [currentExamProblemResult, setCurrentExamProblemResult] = useAtom(atoms.currentExamProblemResultAtom)
-  const resetExamProblemResults = useSetAtom(atoms.resetExamProblemResultsAtom)
+  const dispatch = useAppDispatch();
+  const examProblemResults = useAppSelector(selectExamProblemResults);
+  const examProblemResultsIndex = useAppSelector(selectExamProblemResultsIndex);
+  const currentExamProblemResult = useAppSelector(
+    selectCurrentExamProblemResult,
+  );
 
+  const setExamProblemResults = useCallback(
+    (examProblemResults: ExamProblemResult[]) => {
+      dispatch(setExamProblemResultsAction(examProblemResults));
+    },
+    [dispatch],
+  );
+
+  const setExamProblemResultsIndex = useCallback(
+    (examProblemResultsIndex: number) => {
+      dispatch(setExamProblemResultsIndexAction(examProblemResultsIndex));
+    },
+    [dispatch],
+  );
+
+  const setCurrentExamProblemResult = useCallback(
+    (examProblemResult: ExamProblemResult) => {
+      dispatch(setCurrentProblemResultAction(examProblemResult));
+    },
+    [dispatch],
+  );
+
+  const resetExamProblemResults = useCallback(() => {
+    dispatch(reset());
+  }, [dispatch]);
 
   return {
     examProblemResults,
@@ -15,8 +51,6 @@ export default function useExamProblemResults() {
     setExamProblemResultsIndex,
     currentExamProblemResult,
     setCurrentExamProblemResult,
-    resetExamProblemResults
+    resetExamProblemResults,
   };
-
-  
 }
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -2,12 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import navbarReducer from "@/slices/navbar";
 import problemsReducer from "@/slices/problems";
 import examProblemReducer from "@/slices/examProblems";
+import examProblemResultsReducer from "@/slices/examProblemResults";
+
 export const makeStore = () => {
   const store = configureStore({
     reducer: {
       navbarReducer,
       problemsReducer,
       examProblemReducer,
+      examProblemResultsReducer,
     },
     devTools: process.env.NODE_ENV !== "production",
   });
