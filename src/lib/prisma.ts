5896a09658d913e54a4eeb4346fb4448e499ddb4
Aiden Hong <brain1401@gmail.com>
2024-03-06 23:44:12 +0900

    chore: prisma.ts 파일에서 prisma 인스턴스 생성 방식 변경

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
-
-const prisma = global.prisma ?? prismaClientSingleton();
+const prisma = global.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
