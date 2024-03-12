3e96ae58f70b3918b33f5dc7ad9e9f0c0f5ecf30
Aiden Hong <brain1401@gmail.com>
2024-03-13 04:31:38 +0900

    chore: prettified

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,11 @@
 import { MetadataRoute } from "next";
 
-export default function robots():MetadataRoute.Robots {
+export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
       disallow: "/api/",
-    }
-  }
+    },
+  };
 }
