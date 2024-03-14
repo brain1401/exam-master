26b95ce65eac1de67a214d3d6a63c922e0d89664
Aiden Hong <brain1401@gmail.com>
2024-03-15 03:53:25 +0900

    test: text 크기 변경

--- src/app/components/ui/LoginRequired.tsx
+++ src/app/components/ui/LoginRequired.tsx
@@ -3,7 +3,7 @@ import Announcements from "./Announcements";
 export default function LoginRequired() {
   return (
     <>
-      <h1 className="mx-auto mt-10 w-[20rem] rounded-xl bg-red-500 p-5 text-center text-2xl text-white md:w-fit">{`서비스를 이용하려면 로그인이 필요합니다!`}</h1>
+      <h1 className="mx-auto mt-10 w-[20rem] rounded-xl bg-red-500 p-5 text-center text-3xl text-white md:w-fit">{`서비스를 이용하려면 로그인이 필요합니다!`}</h1>
       <Announcements />
     </>
   );
