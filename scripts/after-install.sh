ed0e21e4012ba2c1c66c2e56aeb9f39a7f778f32
Aiden Hong <brain1401@gmail.com>
2024-03-15 03:44:13 +0900

    chore: node_modules가 없을 때는 생성하도록 변경

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -5,10 +5,12 @@ nvm use 20
 
 cd /home/ubuntu/nextjs/exam-master/
 
-# node_modules 디렉토리가 존재하면 삭제
-# if [ -d "node_modules" ]; then
-#     rm -rf node_modules/
-# fi
+if [ ! -d "node_modules" ]; then
+    echo "node_modules not found, installing dependencies"
+    npm install
+else
+    echo "node_modules exists, skipping dependency installation"
+fi
 
 # npm install 실행
 npm install
