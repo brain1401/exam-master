073e76b7bd03aaac9762a783bd5c2033ed7fd2a1
Aiden Hong <brain1401@gmail.com>
2024-03-15 03:06:34 +0900

    chore: cicd 테스트

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -3,6 +3,7 @@ export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.11.1/bin
 pm2 describe exam-master > /dev/null
 RUNNING=$?
 
+cd /home/ubuntu/nextjs/exam-master/
 if [ "${RUNNING}" -ne 0 ]; then
   pm2 start npm --name exam-master --time -- run start --port=3000
 else
