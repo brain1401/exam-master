44f3f9faf7314aeb7fa7d681b6bcf85e909022ab
Aiden Hong <brain1401@gmail.com>
2024-03-15 02:46:51 +0900

    chore: cicd 테스트

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm use 20
+
 cd /home/ubuntu/nextjs/exam-master/
 
 # node_modules 디렉토리가 존재하면 삭제
