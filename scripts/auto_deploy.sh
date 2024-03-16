4ef67a3f70789e5c2880d848cc99bc3afd49d00b
Aiden Hong <brain1401@gmail.com>
2024-03-16 19:19:35 +0900

    fix: cicd 오류 수정

--- scripts/auto_deploy.sh
+++ scripts/auto_deploy.sh
@@ -57,13 +57,3 @@ done
 # NGINX 구성 업데이트하여 트래픽을 새 컨테이너로 리디렉션
 sed -i "s/localhost:${PREVIOUS_PORT}/localhost:${NEW_PORT}/g" /etc/nginx/sites-available/exammaster
 nginx -s reload
-
-# 리디렉션이 완료되었는지 확인
-HTTPCODE=$(curl --max-time 5 --silent --write-out %{http_code} --output /dev/null https://exammaster.co.kr)
-
-if [ "$HTTPCODE" -eq 200 ]; then
-    echo "배포가 성공적으로 완료되었습니다."
-else
-    echo "배포가 실패했습니다."
-    exit 1
-fi
