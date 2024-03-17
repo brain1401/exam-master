fd00e27fb20ea60d1f4c4b854cd5a1f307b4b6ac
Aiden Hong <brain1401@gmail.com>
2024-03-17 09:55:02 +0900

    chore: cicd 테스트

--- scripts/auto_deploy.sh
+++ scripts/auto_deploy.sh
@@ -3,30 +3,12 @@
 # 현재 서비스의 HTTP 상태 코드 확인
 HTTPCODE=$(curl --max-time 5 --silent --write-out %{http_code} --output /dev/null https://exammaster.co.kr)
 
-# 현재 "blue" 컨테이너의 포트를 조회
-BLUE_PORT=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "3000/tcp") 0).HostPort}}' exam-master-blue)
-if [ -z "$BLUE_PORT" ]; then
-    echo "BLUE_PORT 조회에 실패했습니다."
-    exit 1
-fi
+# NGINX 설정 파일에서 현재 설정된 포트 조회
+PREVIOUS_PORT=$(grep "proxy_pass http://localhost" /etc/nginx/sites-available/exam-master | grep -o '[0-9]\+')
 
-# 현재 "green" 컨테이너의 포트를 조회
-GREEN_PORT=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "3000/tcp") 0).HostPort}}' exam-master-green)
-if [ -z "$GREEN_PORT" ]; then
-    echo "GREEN_PORT 조회에 실패했습니다."
+if [ -z "$PREVIOUS_PORT" ]; then
+    echo "NGINX 설정 파일에서 포트 조회에 실패했습니다."
     exit 1
-fi
-
-if [ "$HTTPCODE" -eq 200 ]; then
-    # "green" 컨테이너 중지 및 제거
-    docker stop exam-master-green
-    docker rm exam-master-green
-    PREVIOUS_PORT=$GREEN_PORT
-else
-    # "blue" 컨테이너 중지 및 제거
-    docker stop exam-master-blue
-    docker rm exam-master-blue
-    PREVIOUS_PORT=$BLUE_PORT
 fi
 
 # 새 컨테이너에 사용할 포트 결정
@@ -70,7 +52,7 @@ until [ "$(docker inspect --format='{{.State.Health.Status}}' exam-master-${NEW}
 done
 
 # NGINX 구성 업데이트하여 트래픽을 새 컨테이너로 리디렉션
-sed -i "s/localhost:${PREVIOUS_PORT}/localhost:${NEW_PORT}/g" /etc/nginx/sites-available/exammaster
+sudo sed -i "s|\(proxy_pass http://localhost:\)${PREVIOUS_PORT}|\1${NEW_PORT}|" /etc/nginx/sites-available/exam-master
 nginx -s reload
 
 # 리디렉션이 완료되었는지 확인
