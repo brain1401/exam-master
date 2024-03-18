0ae027002e09a6f00bb8276b1848f3ae711014b3
Aiden Hong <brain1401@gmail.com>
2024-03-18 23:20:32 +0900

    chore: 무중단 배포 redex 테스트

--- src/app/components/CreateProblems/CreateProblems.tsx
+++ src/app/components/CreateProblems/CreateProblems.tsx
@@ -11,11 +11,11 @@ import ProblemEditorLayout from "../layouts/ProblemEditorLayout";
 export default function CreateProblems() {
   const { resetProblems } = useProblems();
 
-  useEffect(() => {
-    return () => {
-      resetProblems();
-    };
-  }, [resetProblems]);
+  // useEffect(() => {
+  //   return () => {
+  //     resetProblems();
+  //   };
+  // }, [resetProblems]);
 
   return (
     <ProblemEditorLayout>
