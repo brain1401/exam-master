60d04dccecda0e874be2ab6323d4bcbf614871dd
Aiden Hong <brain1401@gmail.com>
2024-03-18 22:24:59 +0900

    chore: 에러 컴포넌트 삭제

--- src/app/api/testError/route.ts
+++ src/app/api/testError/route.ts
@@ -8,7 +8,7 @@ export async function GET(req: NextRequest) {
     return NextResponse.json({ error: "로그인이 필요합니다." }, { status: 401 });
   }
 
-  new Error("에러 발생시키기");
+  throw new Error("에러 발생시키기");
   
   return NextResponse.json({ message: "error test" }, { status: 400 });
 }
