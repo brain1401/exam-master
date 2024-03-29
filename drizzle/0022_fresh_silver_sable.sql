470fffc1360a6f1dfda7e1d4b736fb0b51b79e1c
Aiden Hong <brain1401@gmail.com>
2024-03-29 12:00:39 +0900

    chore: 스키마 변경

---
+++ drizzle/0022_fresh_silver_sable.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "_LikedProblemSets" DROP CONSTRAINT "_LikedProblemSets_problemSetUuid_unique";--> statement-breakpoint
+ALTER TABLE "_LikedProblemSets" DROP CONSTRAINT "_LikedProblemSets_userUuid_unique";
