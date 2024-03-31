b0e0ccc8b0817667645f3c0f3d55d27bd0feaa2e
Aiden Hong <brain1401@gmail.com>
2024-03-31 12:20:53 +0900

    feat: 문제 관리에 서버컴포넌트에서 react query prefetch 적용

--- src/app/manage/page.tsx
+++ src/app/manage/page.tsx
@@ -2,8 +2,13 @@ import { getServerSession } from "next-auth";
 import LoginRequired from "../components/ui/LoginRequired";
 import ProblemSetsPage from "../components/ProblemSets/ProblemSetsPage";
 import type { Metadata } from "next";
-import { getProblemSetsMaxPage } from "@/service/problems";
+import { getProblemSets, getProblemSetsMaxPage } from "@/service/problems";
 import { getUserUUIDbyEmail } from "@/service/user";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
 
 export const metadata: Metadata = {
   title: "문제 관리",
@@ -16,14 +21,25 @@ export default async function ManagePage() {
   if (!session || !session?.user?.email) {
     return <LoginRequired />;
   }
+  const userEmail = session.user.email;
 
-  const userUUID = await getUserUUIDbyEmail(session.user.email);
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["problemSets", 1, 8, false, "", null, userEmail],
+    queryFn: () => getProblemSets(userEmail, "1", "8"),
+  });
+
+  const userUUID = await getUserUUIDbyEmail(userEmail);
   const maxPage = await getProblemSetsMaxPage(false, "", 8, userUUID);
+
   return (
-    <ProblemSetsPage
-      type="manage"
-      userEmail={session.user.email}
-      maxPage={maxPage ?? 1}
-    />
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ProblemSetsPage
+        type="manage"
+        userEmail={userEmail}
+        maxPage={maxPage ?? 1}
+      />
+    </HydrationBoundary>
   );
 }
