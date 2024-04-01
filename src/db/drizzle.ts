9b764cc3dd579541548704bb4ad6b315b001ecf6
Aiden Hong <brain1401@gmail.com>
2024-04-02 01:39:41 +0900

    chore: postgres -> pg 로 변경

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,13 +1,13 @@
-import postgres from "postgres";
+import { Pool } from "pg";
 import * as schema from "./schema";
-import { drizzle } from "drizzle-orm/postgres-js";
+import { drizzle } from "drizzle-orm/node-postgres";
 
 if (process.env.DATABASE_URL === undefined) {
   throw new Error("DATABASE_URL is not defined");
 }
 
-const queryClient = postgres(process.env.DATABASE_URL);
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
-const drizzleSession = drizzle(queryClient, { schema });
+const drizzleSession = drizzle(pool, { schema });
 
 export default drizzleSession;
