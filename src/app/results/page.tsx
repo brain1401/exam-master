1e498affc13febbbfdee1931d3765f756462e46b
Aiden Hong <brain1401@gmail.com>
2024-04-04 08:48:26 +0900

    chore: 원자 Hydrate를 위해 Jotai Provider로 감쌈

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,5 +1,10 @@
 import ResultsPaginationPage from "@/components/pagination/ResultsPaginationPage";
+import JotaiProvider from "@/context/JotaiContext";
 
 export default async function Results() {
-  return <ResultsPaginationPage page={1} />;
+  return (
+    <JotaiProvider>
+      <ResultsPaginationPage page={1} />
+    </JotaiProvider>
+  );
 }
