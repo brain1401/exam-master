bd7c2e8a5ac5137685cad0cdf42ba4d852158305
Aiden Hong <brain1401@gmail.com>
2024-04-03 20:25:28 +0900

    chore: revalidation 추가

--- src/components/CreateProblems/CreateProblems.tsx
+++ src/components/CreateProblems/CreateProblems.tsx
@@ -7,10 +7,12 @@ import CreateProblemsSubmitButton from "./CreateProblemsSubmitButton";
 import { useEffect } from "react";
 import useProblems from "@/hooks/useProblems";
 import ProblemEditorLayout from "../layouts/ProblemEditorLayout";
+import useRevalidate from "@/hooks/useRevalidate";
 
 export default function CreateProblems() {
   const { resetProblems, currentProblem, problemSetIsPublic } = useProblems();
 
+  const { revalidateAllPath } = useRevalidate();
   useEffect(() => {
     return () => {
       resetProblems();
@@ -18,16 +20,20 @@ export default function CreateProblems() {
   }, [resetProblems]);
 
   useEffect(() => {
+    revalidateAllPath();
+  }, [revalidateAllPath]);
+
+  useEffect(() => {
     console.log(currentProblem);
   }, [currentProblem]);
 
   useEffect(() => {
-    console.log("problemSetIsPublic :",problemSetIsPublic);
+    console.log("problemSetIsPublic :", problemSetIsPublic);
   }, [problemSetIsPublic]);
 
   return (
     <ProblemEditorLayout>
-      <ProblemsOption type="create"/>
+      <ProblemsOption type="create" />
 
       <CurrentProblemIndicator />
 
