fbcbc908840e4877d43c80bcc4bcccf873bb2fc3
Aiden Hong <brain1401@gmail.com>
2024-04-06 18:41:01 +0900

    chore: usePreventClose 훅에 useCallback 훅을 사용하여 성능을 개선하고, beforeunload 이벤트 리스너 등록 및 해제하는 부분을 수정

--- src/hooks/usePreventClose.ts
+++ src/hooks/usePreventClose.ts
@@ -1,19 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
 
 export default function usePreventClose() {
-  useEffect(() => {
-    //페이지를 닫거나 새로고침을 할 때 경고창을 띄우는 이벤트 리스너 등록 및 해제
-    function preventClose(e: BeforeUnloadEvent) {
-      e.preventDefault();
-      e.returnValue = ""; //Chrome에서 동작하도록; deprecated
-    }
+  //페이지를 닫거나 새로고침을 할 때 경고창을 띄우는 이벤트 리스너 등록 및 해제
+  const preventClose = useCallback((e: BeforeUnloadEvent) => {
+    e.preventDefault();
+    e.returnValue = ""; //Chrome에서 동작하도록; deprecated
+  }, []);
 
-    (() => {
-      window.addEventListener("beforeunload", preventClose);
-    })();
+  useEffect(() => {
+    window.addEventListener("beforeunload", preventClose);
 
     return () => {
       window.removeEventListener("beforeunload", preventClose);
     };
-  }, []);
+  }, [preventClose]);
 }
