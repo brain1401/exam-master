9cddcc7703027345859f0970349aaa9343072dc9
Aiden Hong <brain1401@gmail.com>
2024-04-09 20:49:52 +0900

    chore: Sentry 이벤트 전송 시 digest 태그 추가

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -6,6 +6,18 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://6f228b3ef81ec4685a448b9bb07255e2@o4506315586011136.ingest.sentry.io/4506315638439941",
+  beforeSend(event, hint) {
+    const error = hint.originalException as Error & { digest?: string };
+
+    if (error?.digest) {
+      event.tags = {
+        ...event.tags,
+        digest: error.digest,
+      };
+    }
+
+    return event;
+  },
   enabled: process.env.NODE_ENV === "production",
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
