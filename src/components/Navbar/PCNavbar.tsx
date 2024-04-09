716890b49b6bcba911ec537ec4a38e6e72746288
Aiden Hong <brain1401@gmail.com>
2024-04-09 20:36:45 +0900

    chore: navbar 개선

--- src/components/Navbar/PCNavbar.tsx
+++ src/components/Navbar/PCNavbar.tsx
@@ -17,40 +17,42 @@ export default function PCNavbar({ loginButton }: Props) {
 
   return (
     <nav // navbar
-      className="transparent z-30 flex w-full border-b border-gray-300 bg-main"
+      className="z-30 flex h-[3.6rem] w-full justify-center border-b border-gray-300"
     >
-      <div className="flex w-full items-center justify-between md:gap-3">
-        <div className="flex">
-          <div className="ml-[1rem] px-5 py-2 text-xl font-bold md:ml-[3rem] md:text-2xl">
-            <Link href="/">
-              <Image src={logo} alt="logo" className="h-[2rem] w-[8rem]" />
-            </Link>
+      <div className="flex w-full max-w-[110rem] items-center justify-center">
+        <div className="flex w-full items-center justify-between md:gap-3">
+          <div className="flex">
+            <div className="ml-[1rem] px-5 py-2 text-xl font-bold md:ml-[3rem] md:text-2xl">
+              <Link href="/">
+                <Image src={logo} alt="logo" className="h-[2rem] w-[8rem]" />
+              </Link>
+            </div>
+            <ul className="flex items-center gap-3">
+              <li className={LIST_ITEM}>
+                <Link href="/exam">내 문제 풀기</Link>
+              </li>
+              <li className={LIST_ITEM}>
+                <Link href="/manage">내 문제 관리</Link>
+              </li>
+              <li className={LIST_ITEM}>
+                <Link href="/create">문제 생성</Link>
+              </li>
+              <li className={LIST_ITEM}>
+                <Link href="/results">시험 결과</Link>
+              </li>
+            </ul>
           </div>
-          <ul className="flex items-center gap-3">
-            <li className={LIST_ITEM}>
-              <Link href="/exam">내 문제 풀기</Link>
-            </li>
-            <li className={LIST_ITEM}>
-              <Link href="/manage">내 문제 관리</Link>
-            </li>
-            <li className={LIST_ITEM}>
-              <Link href="/create">문제 생성</Link>
-            </li>
-            <li className={LIST_ITEM}>
-              <Link href="/results">시험 결과</Link>
-            </li>
-          </ul>
-        </div>
 
-        <div className="hidden md:mr-[10vw] md:block ">
-          <div className="flex h-full w-full items-center justify-center">
-            {loginButton}
+          <div className="hidden md:mr-[10vw] md:block ">
+            <div className="flex h-full w-full items-center justify-center">
+              {loginButton}
+            </div>
           </div>
-        </div>
-        <div className="md:hidden">
-          <button onClick={() => toggleMobileMenu()}>
-            <HiMenu className="absolute right-5 top-[0.5rem] text-[1.9rem]" />
-          </button>
+          <div className="md:hidden">
+            <button onClick={() => toggleMobileMenu()}>
+              <HiMenu className="absolute right-5 top-[0.5rem] text-[1.9rem]" />
+            </button>
+          </div>
         </div>
       </div>
     </nav>
