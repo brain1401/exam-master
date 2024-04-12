983a777c2ae31d92a07f6f81370082c389c56885
Aiden Hong <brain1401@gmail.com>
2024-04-12 12:59:11 +0900

    fix: 시험 결과 창에서 입력한 답 에러 수정

--- src/components/result/SubjectiveAnswered.tsx
+++ src/components/result/SubjectiveAnswered.tsx
@@ -4,13 +4,17 @@ import useProblemResults from "@/hooks/useProblemResults";
 
 export default function SubjectiveAnswered() {
   const {
-    currentExamResult: { subjectiveAnswered },
+    currentExamResult: { subjectiveAnswered, correctCandidates },
   } = useProblemResults();
 
   return (
     <>
       {
-        <p className="text-lg">{`입력한 답 : ${subjectiveAnswered || "답을 입력하지 않음."}`}</p>
+        <p className="text-lg">
+          {correctCandidates?.length && correctCandidates.length > 0
+            ? ""
+            : `입력한 답 : ${subjectiveAnswered || "답을 입력하지 않았습니다."}`}
+        </p>
       }
     </>
   );
