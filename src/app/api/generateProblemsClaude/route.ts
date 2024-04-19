bbc4c075e66528fb7f04441139ad5acfcaa8b1ab
Aiden Hong <brain1401@gmail.com>
2024-04-19 17:09:07 +0900

    chore: claude 모델 문자열 모듈화

--- src/app/api/generateProblemsClaude/route.ts
+++ src/app/api/generateProblemsClaude/route.ts
@@ -10,14 +10,13 @@ import {
   systemPrompt,
 } from "@/prompt/problemGeneration";
 import { generateQuestions } from "@/service/generate";
+import { claudeOpus } from "@/const/bedrockClaudeModel";
 
-const sonnet = "anthropic.claude-3-sonnet-20240229-v1:0";
-const opus = "anthropic.claude-3-opus-20240229-v1:0";
 
 const model = new BedrockChat({
   temperature: 0.4,
   region: "us-west-2",
-  model: opus,
+  model: claudeOpus,
   maxTokens: 4000,
   credentials: {
     accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID || "",
