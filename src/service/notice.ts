4e8d4461918678e6445b676b8cd1bd029970ca67
Aiden Hong <brain1401@gmail.com>
2024-04-23 09:29:05 +0900

    chore: 문제 수정

--- src/service/notice.ts
+++ src/service/notice.ts
@@ -7,6 +7,9 @@ export async function noticeToPhone({
   title: string;
   body: string;
 }) {
+  const key = process.env.PHONE_NOTICE_KEY;
+  if (!key) throw new Error("PHONE_NOTICE_KEY is not defined");
+
   await axios.get(
     `https://asia-northeast3-noti-lab-production.cloudfunctions.net/api/notification/v1/notification?nickname=Aiden&title=${encodeURIComponent(title)}&body=${encodeURIComponent(body)}&secretKey=${process.env.PHONE_NOTICE_KEY}`,
   );
