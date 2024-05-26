45dfff62a1b71a6de13f5909b2369f9fe466bd68
Aiden Hong <brain1401@gmail.com>
2024-05-26 18:57:12 +0900

    fix: 문제 풀 때 문제 이동 시 주관식 문제 입력칸 저장 안되던 문제 해결

--- src/components/exam/ExamProblem.tsx
+++ src/components/exam/ExamProblem.tsx
@@ -77,6 +77,7 @@ export default function ExamProblem({
       onChange={(e) => {
         setCurrentExamProblemSubAnswer(e.target.value);
       }}
+      value={currentProblem.subAnswer ?? ""}
     />
   );
 
