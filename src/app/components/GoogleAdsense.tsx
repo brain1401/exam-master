c7056e976408c864651f82aa8af79771f7649be3
Aiden Hong <brain1401@gmail.com>
2024-10-10 09:16:35 +0900

    chore: 애드센스 관련 개선

--- src/app/components/GoogleAdsense.tsx
+++ src/app/components/GoogleAdsense.tsx
@@ -4,6 +4,7 @@ export default function GoogleAdsense() {
   return (
     <Script
       async
+      strategy="lazyOnload"
       crossOrigin="anonymous"
       src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_ID}`}
     />
