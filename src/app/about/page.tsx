c9dc5cd6809d895040b2625af257778b609c21ea
Aiden Hong <brain1401@gmail.com>
2024-10-13 05:29:54 +0900

    chore: 패딩 개선

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import Announcements from "@/components/ui/Announcements";
 
 export default function page() {
   return (
-    <div className="flex flex-1 justify-center items-center p-4">
+    <div className="flex flex-1 justify-center items-center p-4 md:py-10">
       <div className="w-full max-w-2xl">
         <Announcements />
       </div>
