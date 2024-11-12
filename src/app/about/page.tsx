f42f808c619cd68757f5dd0c869043dc0d502adc
Aiden <brain1401@gmail.com>
2024-11-12 18:14:50 +0900

    chore: about 경로 정적 경로로 처리

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,11 @@
 import Announcements from "@/components/ui/Announcements";
 
+// about 경로는 모든 사용자에게 동일한 페이지를 제공하므로 스태틱 처리
+export const dynamic = "force-static";
+
 export default function page() {
   return (
-    <div className="flex flex-1 justify-center items-center p-4 md:py-10">
+    <div className="flex flex-1 items-center justify-center p-4 md:py-10">
       <div className="w-full max-w-2xl">
         <Announcements />
       </div>
